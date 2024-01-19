32c9e303eec8facd6580fb564e0016458a9b8a63
Rajdeep <snnbarot@gmail.com>
2024-01-19 17:23:37 +0530

    Updated slider with logo grid

--- components/ui/LogoGrid/index.jsx
+++ components/ui/LogoGrid/index.jsx
@@ -1,46 +1,31 @@
-import Image from 'next/image'
-import BP from '../../../public/logos/Bhagyoday Pumps Pvt. Ltd Greyscale.png'
-// import PWT from '../../../public/logos/pwt.svg'
-import AR from '../../../public/logos/AR gradient.svg'
-import KK from '../../../public/logos/kk gradient.svg'
-import struct from '../../../public/logos/Layer 4.svg'
-/*
-    KK
-    AR
-    PWT
-    BP
-    fnc101
-    struct
-*/
-
-// array of images
-const logos = [
-    {
-        src: KK,
-        alt: "Karma Kinesis"
-    },
-    // {
-    //     src: PWT,
-    //     alt: "sendgrid"
-    // },
-    {
-        src: BP,
-        alt: "layers"
-    },
-    {
-        src: AR,
-        alt: "adobe"
-    },
-    {
-        src: struct,
-        alt: "struct"
-    }
-]
-
-
 const LogoGrid = () => (
     <div>
-        <div className="custom-screen">
+        <div class="slider">
+            <div class="slide-track">
+                <div class="slide">
+                    <img src="/logogridTCD.svg" alt="logo aler" />
+                </div>
+                <div class="slide">
+                    <img src="/logogridTCD.svg" alt="logo aler" />
+                </div>
+                <div class="slide">
+                    <img src="/logogridTCD.svg" alt="logo aler" />
+                </div>
+                <div class="slide">
+                    <img src="/logogridTCD.svg" alt="logo aler" />
+                </div>
+                <div class="slide">
+                    <img src="/logogridTCD.svg" alt="logo aler" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+)
+
+export default LogoGrid;
+
+{/* <div className="custom-screen">
             <h2 className="font-semibold text-sm text-gray-600 text-center">
                 TRUSTED BY TEAMS FROM AROUND THE WORLD
             </h2>
@@ -55,52 +40,4 @@ const LogoGrid = () => (
                     }
                 </ul>
             </div>
-        </div>
-        {/* <div class="slider">
-            <div class="slide-track">
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
-                </div>
-                <div class="slide">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
-                </div>
-            </div>
         </div> */}
-    </div>
-
-)
-
-export default LogoGrid
